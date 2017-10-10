4551f4de8ffa5116d8601a1aabe35865f07eeb74
Hagezawa <hagezawavita@gmail.com>
2017-10-10 19:04:06 +0900

    上げ忘れ

--- app/models/Message.java
+++ app/models/Message.java
@@ -1,4 +1,22 @@
 package models;
 
-public class Message {
+import java.util.Date;
+import java.util.List;
+
+import javax.persistance.*;
+import javax.validation.*;
+
+import com.sun.javafx.beans.IDProperty;
+import play.db.ebean.*;
+import play.data.validation.*;
+
+@Entity
+public class Message extends Model {
+
+    @Id
+    public Long id;
+    public String name;
+    public String mail;
+    public String message;
+    public Date postdate;
 }
